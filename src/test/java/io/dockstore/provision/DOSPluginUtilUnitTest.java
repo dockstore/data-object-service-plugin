8868fbbee2378b077ac1d1627720a951dc9078c9
Walt Shands <jshands@ucsc.edu>
2018-07-16 14:21:04 -0700

    use expected test response from live endpoint

--- src/test/java/io/dockstore/provision/DOSPluginUtilUnitTest.java
+++ src/test/java/io/dockstore/provision/DOSPluginUtilUnitTest.java
@@ -106,25 +106,52 @@ public class DOSPluginUtilUnitTest {
     @Test
     public void testReadResponse() throws IOException {
         ImmutableTriple<String, String, String> split =
-                new ImmutableTriple<>("dos", "ec2-52-26-45-130.us-west-2.compute.amazonaws.com:8080", "911bda59-b6f9-4330-9543-c2bf96df1eca");
+                new ImmutableTriple<>("dos", "dataguids.org", "dg.4503/630d31c3-381e-488d-b639-ce5d047a0142");
+        System.out.println("testReadResponse: createConnection for: " + split.getLeft() + split.getMiddle() + split.getRight());
+        HttpURLConnection actualConn = pluginUtil.createConnection("https", split);
+        InputStream expectedResponse = IOUtils.toInputStream(
+        "{" +
+                "\"data_object\": {" +
+                        "\"checksums\": [" +
+                        "{" +
+                                "\"checksum\": \"3b0f63a815384a3d44c61b4abd40caf9\", " +
+                                        "\"type\": \"md5\"" +
+                        "}" +
+                        "], " +
+                        "\"created\": \"2018-05-26T13:43:15.070662\", " +
+                        "\"description\": \"\", " +
+                        "\"id\": \"dg.4503/630d31c3-381e-488d-b639-ce5d047a0142\", " +
+                        "\"mime_type\": \"\", " +
+                        "\"name\": null, " +
+                        "\"size\": 2201638, " +
+                        "\"updated\": \"2018-05-26T13:43:15.070672\", " +
+                        "\"urls\": [" +
+                        "{" +
+                                "\"url\": \"gs://cgp-commons-multi-region-public/topmed_open_access/44a8837b-4456-5709-b56b-54e23000f13a/NWD100953.recab.cram.crai\"" +
+                        "}, " +
+                        "{" +
+                                "\"url\": \"s3://cgp-commons-public/topmed_open_access/44a8837b-4456-5709-b56b-54e23000f13a/NWD100953.recab.cram.crai\"" +
+                        "}" +
+                        "], " +
+                        "\"version\": \"2ba0ac25\"" +
+                "}" +
+        "}");
 
-        HttpURLConnection actualConn = pluginUtil.createConnection("http", split);
-
-        InputStream expectedResponse = IOUtils.toInputStream("{  \"data_object\": {    \"aliases\": [      " +
-                "\"phase3\",      \"data\",      \"HG03237\",      \"cg_data\",      \"ASM_blood\",      \"REPORTS\"," +
-                "      \"substitutionLengthCoding-GS000017140-ASM.tsv\",      " +
-                "\"phase3/data/HG03237/cg_data/ASM_blood/REPORTS/substitutionLengthCoding-GS000017140-ASM.tsv\"    ]," +
-                "    \"checksums\": [      {        \"checksum\": \"ff7d7ec9a803e09ffab681165a9b7c36\",        " +
-                "\"type\": \"md5\"      }    ],    \"created\": \"2015-05-21T23:09:20+00:00\",    \"current\": true, " +
-                "   \"id\": \"911bda59-b6f9-4330-9543-c2bf96df1eca\",    \"size\": \"491\",    \"updated\": " +
-                "\"2015-05-21T23:09:20+00:00\",    \"urls\": [      {        \"system_metadata\": {          " +
-                "\"StorageClass\": \"STANDARD\",          \"bucket_name\": \"1000genomes\",          \"event_type\": " +
-                "\"ObjectCreated:Put\"        },        \"url\": " +
-                "\"s3://1000genomes/phase3/data/HG03237/cg_data/ASM_blood/REPORTS/substitutionLengthCoding" +
-                "-GS000017140-ASM.tsv\",        \"user_metadata\": {          \"s3cmd-attrs\": " +
-                "\"uid:5343/gname:sysadmin/uname:meslerd/gid:14/mode:33188/mtime:1432249758/atime:1432249758/md5" +
-                ":ff7d7ec9a803e09ffab681165a9b7c36/ctime:1432249758\"        }      }    ],    \"version\": " +
-                "\"2018-05-01T05:44:57.781598Z\"  }}", "UTF-8");
+//        InputStream expectedResponse = IOUtils.toInputStream("{  \"data_object\": {    \"aliases\": [      " +
+//                "\"phase3\",      \"data\",      \"HG03237\",      \"cg_data\",      \"ASM_blood\",      \"REPORTS\"," +
+//                "      \"substitutionLengthCoding-GS000017140-ASM.tsv\",      " +
+//                "\"phase3/data/HG03237/cg_data/ASM_blood/REPORTS/substitutionLengthCoding-GS000017140-ASM.tsv\"    ]," +
+//                "    \"checksums\": [      {        \"checksum\": \"ff7d7ec9a803e09ffab681165a9b7c36\",        " +
+//                "\"type\": \"md5\"      }    ],    \"created\": \"2015-05-21T23:09:20+00:00\",    \"current\": true, " +
+//                "   \"id\": \"911bda59-b6f9-4330-9543-c2bf96df1eca\",    \"size\": \"491\",    \"updated\": " +
+//                "\"2015-05-21T23:09:20+00:00\",    \"urls\": [      {        \"system_metadata\": {          " +
+//                "\"StorageClass\": \"STANDARD\",          \"bucket_name\": \"1000genomes\",          \"event_type\": " +
+//                "\"ObjectCreated:Put\"        },        \"url\": " +
+//                "\"s3://1000genomes/phase3/data/HG03237/cg_data/ASM_blood/REPORTS/substitutionLengthCoding" +
+//                "-GS000017140-ASM.tsv\",        \"user_metadata\": {          \"s3cmd-attrs\": " +
+//                "\"uid:5343/gname:sysadmin/uname:meslerd/gid:14/mode:33188/mtime:1432249758/atime:1432249758/md5" +
+//                ":ff7d7ec9a803e09ffab681165a9b7c36/ctime:1432249758\"        }      }    ],    \"version\": " +
+//                "\"2018-05-01T05:44:57.781598Z\"  }}", "UTF-8");
         HttpURLConnection mockConn = Mockito.mock(HttpURLConnection.class);
         Mockito.when(mockConn.getInputStream()).thenReturn(expectedResponse);
         BufferedReader bufferedReader = Mockito.spy(new BufferedReader(new InputStreamReader(mockConn.getInputStream ())));
